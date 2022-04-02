7baf1494bbd853d72c72089194a9be6f9892f383
UgadiBhargavi <ugadi.bhargavi@jukshio.com>
2022-04-02 19:47:04 +0530

    first commit

---
+++ mylibrary/src/main/java/com/example/mylibrary/Add.java
@@ -0,0 +1,8 @@
+package com.example.mylibrary;
+
+public class Add {
+    public static void add(int a, int b,addApiCall addApiCall1){
+        int sum=a+b;
+        addApiCall1.response(sum);
+    }
+}
---
+++ mylibrary/src/main/java/com/example/mylibrary/addApiCall.java
@@ -0,0 +1,5 @@
+package com.example.mylibrary;
+
+public interface addApiCall {
+    void response(int result);
+}
